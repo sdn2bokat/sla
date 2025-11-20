d92512b628fc3979b9ea5d72ba78d577c4520679
sdn2bokat <sdnegeri02bokat@gmail.com>
2025-11-21 01:59:40 +0800

    fix chromium final

--- src/components/VersiWebApp/VersiWebApp.tsx
+++ src/components/VersiWebApp/VersiWebApp.tsx
@@ -5,7 +5,7 @@ export default function VersiWebApp() {
       {/* versi */}
       <div className="text-center text-xs text-gray-500 leading-tight">
         © {new Date().getFullYear()} SD Negeri 2 Bokat<br />
-        SiLALah versi 1.2.1<br />
+        SiLALah versi 1.2.2<br />
         <b className="bg-gray-600 text-white px-[2px] text-[7px] sm:text-[9px]">POWERED BY</b> 
         <b className="text-gray-500 text-[8px] md:text-[10px]"> Sistem Digitalisasi</b>
       </div>
